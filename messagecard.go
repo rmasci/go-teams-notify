272087aec0000ce296760a69fa26fa846c0953fe
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-29 12:36:31 -0500

    Disable debug logging in method calls, found the bug elsewhere. Need to focus on FormatAsCode... methods next

--- messagecard.go
+++ messagecard.go
@@ -20,20 +20,20 @@ const (
 // MessageCard.
 func (mc *MessageCard) AddSection(section ...MessageCardSection) {
 
-	logger.Printf("Existing sections: %+v\n", mc.Sections)
-	logger.Printf("Incoming sections: %+v\n", section)
+	//logger.Printf("Existing sections: %+v\n", mc.Sections)
+	//logger.Printf("Incoming sections: %+v\n", section)
 	mc.Sections = append(mc.Sections, section...)
-	logger.Printf("Sections after append() call: %+v\n", mc.Sections)
+	//logger.Printf("Sections after append() call: %+v\n", mc.Sections)
 }
 
 // AddFact adds one or many additional MessageCardSectionFact values to a
 // MessageCardSection
 func (mcs *MessageCardSection) AddFact(fact ...MessageCardSectionFact) {
 
-	logger.Printf("Existing sections: %+v\n", mcs.Facts)
-	logger.Printf("Incoming sections: %+v\n", fact)
+	//logger.Printf("Existing sections: %+v\n", mcs.Facts)
+	//logger.Printf("Incoming sections: %+v\n", fact)
 	mcs.Facts = append(mcs.Facts, fact...)
-	logger.Printf("Facts after append() call: %+v\n", mcs.Facts)
+	//logger.Printf("Facts after append() call: %+v\n", mcs.Facts)
 
 }
 
