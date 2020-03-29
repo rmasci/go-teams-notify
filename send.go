3a0b28c509811532567ec6178b7f11130e3751ef
Adam Chalkley <atc0005@users.noreply.github.com>
2020-03-29 11:07:45 -0500

    Test returning a pointer to MessageCardSection via constructor

--- send.go
+++ send.go
@@ -254,9 +254,11 @@ func NewMessageCard() *MessageCard {
 }
 
 // NewMessageCardSection creates an empty message card section
-func NewMessageCardSection() MessageCardSection {
+func NewMessageCardSection() *MessageCardSection {
 
-	return MessageCardSection{}
+	msgCardSection := MessageCardSection{}
+
+	return &msgCardSection
 
 }
 
